525e1dce26879008d0381e5d5d3dbe73d1cc32e0
Retno Wulandari <retno22.wulandari@gmail.com>
2020-09-07 22:15:56 +0700

    make dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>SIQI</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,22 +21,17 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <a class="nav-link" href="{{ url('/home') }}">
-                   Home
-                </a>
-                <a class="nav-link" href="{{ url('/page') }}">
-                    Page
-                </a>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="container">                        
+                <div class="navbar-header pull-left">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="storage/logo.png" width="35" height="30" class="d-inline-block align-top" alt="">
+                        SIAKAD SQI UNJ
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                </div>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -78,18 +73,61 @@
                 </div>
             </div>
         </nav>
+        <main class="main-container">
+            <div id= "sidebar" class="sidebar-container">
+            <script type="text/javascript">
+				try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+			</script>
+              <ul class="sidebar-navigation">
+                <li>
+                  <a href="#">
+                  <img src="storage/logo.png" width="25" height="20"> Beranda
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-tachometer" aria-hidden="true"></i> Profil
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-users" aria-hidden="true"></i> Rencana Studi
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-cog" aria-hidden="true"></i> Hasil Studi
+                  </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-info-circle" aria-hidden="true"></i> Laporan KBM
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-info-circle" aria-hidden="true"></i> Pembayaran
+                    </a>
+                </li>
+                </ul>
+                <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
+					<i class="ace-icon fa fa-angle-double-left" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+				</div>
+				<script type="text/javascript">
+					try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+				</script>
+            </div>
 
-        <main class="py-4">
-            <div class='row'>
-                <div class='col-4'>
-                    <div class='container'>
-                        Sidebar
+                <div class="content-container">
+
+                <div class="container-fluid">
+
+                    <!-- Main component for a primary marketing message or call to action -->
+                    <div class="jumbotron">
+                        @yield('content')
                     </div>
                 </div>
-                <div class='col-8'>
-                    @yield('content')
                 </div>
-            </div>
         </main>
     </div>
 </body>
